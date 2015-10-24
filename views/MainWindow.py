44396db18fab1850a29cd1823397abf6e914b9fe
Frank Krick <frank.krick@gmail.com>
2015-10-23 20:38:41 -0400

    Added tabs to embed views in main window

---
+++ views/MainWindow.py
@@ -0,0 +1,32 @@
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+
+import views
+import model
+import controller
+
+
+class MainWindow(QtGui.QMainWindow):
+    def __init__(self, itk):
+        super(MainWindow, self).__init__()
+        self._setup_window()
+        self._create_tabs(itk)
+
+    def _create_tabs(self, itk):
+        tabs = QtGui.QTabWidget()
+        device_graph_editor = views.DeviceGraphEditor(itk)
+        tabs.addTab(device_graph_editor, 'Device Graph Editor')
+        self.setCentralWidget(tabs)
+
+    def _setup_window(self):
+        self.setWindowTitle('Instrument Tool Kit')
+        self.resize(1200, 600)
+        self.move(70, 50)
+
+    def create_button(self):
+        button = QtGui.QPushButton('Quit', self)
+        button.resize(button.sizeHint())
+        button.clicked.connect(self.button_clicked)
+
+    def button_clicked(self):
+        QtCore.QCoreApplication.instance().quit()
